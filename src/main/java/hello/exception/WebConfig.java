a51481e70537e60672461b8f49fd89b5331d3201
Cmk1031 <maple1031@naver.com>
2025-04-19 16:39:34 +0900

    API 예외 처리 - HandlerExceptionResolver 시작

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,18 +1,26 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.Filter;
 
 import org.springframework.boot.web.servlet.DispatcherType;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 import static org.springframework.boot.web.servlet.DispatcherType.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
 
     /*@Bean
     public FilterRegistrationBean logFilter() {
